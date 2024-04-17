8e4b35ea7c7a953634920117e4a567b055254cf0
Sarah Adewale <sarahtemitopeadewale@gmail.com>
2024-04-17 21:55:08 +0100

    feat: MongoDB Database Integration Description: creates merchant & customer database. connects mongo uri to app. creates docker file

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,8 +1,5 @@
-import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';
 import httpProxy from 'http-proxy';
-dotenv.config();
 
 
 const port = process.env.PORT || 4000;
@@ -10,8 +7,6 @@ const port = process.env.PORT || 4000;
 const proxy = httpProxy.createProxyServer();
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('API is running!');
---
+++ auth/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path from 'path'
+import dotenv from 'dotenv';
+import connectDB from "../config/db.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "../routes/userRoutes.js";
+
+
+dotenv.config()
+
+const port = process.env.PORT || 4200;
+
+connectDB()
+const app = express();
+
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// cookie parser middleware
+app.use(cookieParser())
+
+app.use('/api/users', userRoutes)
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}!`);
+});
