387bad5aa7e31aebc683ffe8043e2e4ec0d91e2b
Sarah Adewale <sarahtemitopeadewale@gmail.com>
2024-04-21 16:40:40 +0100

    feat: cors integration description: Included cors on all microservices.

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,22 +1,30 @@
 import express from "express";
-// import path from 'path'
+import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from "../config/db.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
-
 dotenv.config()
 
 const port = process.env.PORT || 4200;
 
 connectDB()
+
+// Allow requests from localhost
+const corsOptions = {
+    origin: 'http://localhost:4200', // Replace 3000 with your client's port if different
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
 const app = express();
 
 // body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors(corsOptions));
 
 // cookie parser middleware
 app.use(cookieParser())
